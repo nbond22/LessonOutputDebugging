b8476b9434c43dec54248b3abb56bb5936a63ff4
Nicole Bond <nicbond22@gmail.com>
2025-04-20 14:26:27 -0400

    Initial Commit

---
+++ LessonOutputDebugging/LessonOutputDebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonOutputDebuggingApp.swift
+//  LessonOutputDebugging
+//
+//  Created by Scholar on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct LessonOutputDebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
